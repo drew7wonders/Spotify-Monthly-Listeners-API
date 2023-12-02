3bc2348bacba91ee2f73efd6905c647c873a0040
Shubh Kesharwani <95687419+drew7wonders@users.noreply.github.com>
2023-12-03 00:00:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
+const cors = require('cors');  // Added for CORS support
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;  // Use the provided port or default to 3000
+
+// Enable CORS for all routes
+app.use(cors());
 
 // Define an API endpoint for scraping monthly listeners
 app.get('/scrape/:artistId', async (req, res) => {
@@ -19,7 +23,8 @@ app.get('/scrape/:artistId', async (req, res) => {
 
   } catch (error) {
     console.error('Error:', error.message);
-    // Return an error response
+
+    // Return a more general error message in the response
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
@@ -27,7 +32,14 @@ app.get('/scrape/:artistId', async (req, res) => {
 // Function to scrape monthly listeners from a Spotify artist page using Puppeteer
 async function scrapeMonthlyListeners(artistUrl) {
   try {
-    const browser = await puppeteer.launch();
+    // Use the provided CHROMIUM_PATH environment variable or default to '/usr/bin/chromium-browser'
+    const chromiumPath = process.env.CHROMIUM_PATH || '/usr/bin/chromium-browser';
+
+    const browser = await puppeteer.launch({
+      headless: 'new',
+      executablePath: chromiumPath,
+    });
+
     const page = await browser.newPage();
 
     await page.goto(artistUrl);
@@ -40,6 +52,8 @@ async function scrapeMonthlyListeners(artistUrl) {
 
   } catch (error) {
     console.error('Error:', error.message);
+
+    // Log the detailed error for your reference
     throw error;
   }
 }
