2d2fa27acfe59fa8bd0d715a4bfece49588c837f
Shubh Kesharwani <95687419+drew7wonders@users.noreply.github.com>
2023-12-03 01:24:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
 const express = require('express');
-const axios = require('axios');
-const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
-const cors = require('cors');  // Added for CORS support
+const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;  // Use the provided port or default to 3000
+const port = process.env.PORT || 3000;
 
-// Enable CORS for all routes
 app.use(cors());
 
-// Define an API endpoint for scraping monthly listeners
 app.get('/scrape/:artistId', async (req, res) => {
   try {
     const artistUrl = `https://open.spotify.com/artist/${req.params.artistId}`;
     const monthlyListeners = await scrapeMonthlyListeners(artistUrl);
-    console.log('Monthly Listeners:', monthlyListeners); // Log the value
+    console.log('Monthly Listeners:', monthlyListeners);
     res.json({ monthlyListeners });
   } catch (error) {
     console.error('Error:', error.message);
@@ -23,15 +19,10 @@ app.get('/scrape/:artistId', async (req, res) => {
   }
 });
 
-// Function to scrape monthly listeners from a Spotify artist page using Puppeteer
 async function scrapeMonthlyListeners(artistUrl) {
   try {
-    // Use the provided CHROMIUM_PATH environment variable or default to '/usr/bin/chromium-browser'
-    const chromiumPath = process.env.CHROMIUM_PATH || '/usr/bin/chromium-browser';
-
     const browser = await puppeteer.launch({
-      headless: 'new',
-      executablePath: puppeteer.executablePath(),
+      headless: true,
     });
 
     const page = await browser.newPage();
@@ -46,13 +37,10 @@ async function scrapeMonthlyListeners(artistUrl) {
 
   } catch (error) {
     console.error('Error:', error.message);
-
-    // Log the detailed error for your reference
     throw error;
   }
 }
 
-// Start the Express server
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
